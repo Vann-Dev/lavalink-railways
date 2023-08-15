fd146a8a0927bc71c04d78768e1c5745ec064840
Bastich <61458683+Bastich-dev@users.noreply.github.com>
2023-08-16 01:06:11 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-curl "https://cdn.darrennathanael.com/jars/Lavalink.jar" -o Lavalink.jar
+curl "https://github.com/lavalink-devs/Lavalink/releases/download/3.7.8/Lavalink.jar" -o Lavalink.jar
 
 java -jar Lavalink.jar
